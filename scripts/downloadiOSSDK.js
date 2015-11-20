7c5da7d1cd42642da5f9eea5f50a1a599560f6b5
IjzerenHein <hrutjes@gmail.com>
2015-11-20 11:26:15 +0100

    Updated to latest OpenTok iOS SDK (2.6.1) (fixes ask for microphone access issue)

--- scripts/downloadiOSSDK.js
+++ scripts/downloadiOSSDK.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 module.exports = function (context) {
-    var IosSDKVersion = 'OpenTok-iOS-2.4.0';
+    var IosSDKVersion = 'OpenTok-iOS-2.6.1';
     var fs = require('fs');
     var downloadFile = require('./downloadFile.js'),
         exec = require('./exec/exec.js'),
@@ -22,14 +22,17 @@ module.exports = function (context) {
                 console.log('downloaded');
                 exec('tar -zxvf ./' + IosSDKVersion + '.tar.bz2', function (err, out, code) {
                     console.log('expanded');
-                    exec('mv ./' + IosSDKVersion + '/OpenTok.framework ' + frameworkDir, function (err, out, code) {
-                        console.log('moved OpenTok.framework into ' + frameworkDir);
-                        exec('rm -r ./' + IosSDKVersion, function (err, out, code) {
-                            console.log('Removed extracted dir');
-                            exec('rm ./' + IosSDKVersion + '.tar.bz2', function (err, out, code) {
-                                exec('touch ' + frameworkDownloaded, function (err) {
-                                    console.log('Removed downloaded SDK');
-                                    deferral.resolve();
+                    exec('rm -r ' + frameworkDir + '/OpenTok.framework', function (err, out, code) {
+                        console.log('removed old framework');
+                        exec('mv ./' + IosSDKVersion + '/OpenTok.framework ' + frameworkDir, function (err, out, code) {
+                            console.log('moved OpenTok.framework into ' + frameworkDir);
+                            exec('rm -r ./' + IosSDKVersion, function (err, out, code) {
+                                console.log('Removed extracted dir');
+                                exec('rm ./' + IosSDKVersion + '.tar.bz2', function (err, out, code) {
+                                    exec('touch ' + frameworkDownloaded, function (err) {
+                                        console.log('Removed downloaded SDK');
+                                        deferral.resolve();
+                                    });
                                 });
                             });
                         });
